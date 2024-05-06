372de39542e182309aaac4c17c029550f6319a9b
marcofilip <129083737+marcofilip@users.noreply.github.com>
2024-05-06 21:47:06 +0000

    Implement tag-based filtering, improved GUI, and fix some minor bugs

--- radio/src/views/HomeView.vue
+++ radio/src/views/HomeView.vue
@@ -3,36 +3,36 @@
   <v-container fluid style="padding-left: 100px; padding-right: 100px">
 
     <!-- Titolo -->
-    <v-row class="margin">
-
-      <v-toolbar>
-        <v-toolbar-title>
-          <b>Radio Player Italia</b>
-        </v-toolbar-title>
-      </v-toolbar>
-
+    <v-row class="justify-center">
+      <v-card class="big-card">
+        <v-card-title class="text-center title big-title">
+          Radio Player Italia
+          <img src="../../img/italy.png" alt="Italian Flag" class="flag"
+            style="width:30px; border: 2px solid white; border-radius: 100%">
+        </v-card-title>
+      </v-card>
     </v-row>
 
     <!-- Filtro -->
-    <v-row justify="center" class="margin">
+    <v-row style="width:800px" align="center" justify="center">
+      <v-btn-toggle v-model="toggle" divided class="margin">
+        <v-btn variant="tonal" class="filter custom-disabled gray-icon" icon="mdi-filter" disabled></v-btn>
+        <v-btn variant="tonal" class="filter" icon="mdi-radio" @click="FilterMode('all')"></v-btn>
+        <v-btn variant="tonal" class="filter" icon="mdi-heart" @click="FilterMode('favorite')"></v-btn>
+      </v-btn-toggle>
 
-      <v-item-group>
-
-        <v-btn-toggle v-model="toggle" divided>
-
-          <v-btn variant="tonal" class="filter custom-disabled" icon="mdi-filter" disabled></v-btn>
-          <v-btn variant="tonal" class="filter" icon="mdi-radio" @click="FilterMode('all')"></v-btn>
-          <v-btn variant="tonal" class="filter" icon="mdi-heart" @click="FilterMode('favorite')"></v-btn>
-
-        </v-btn-toggle>
-
-      </v-item-group>
+      <v-combobox clearable v-model="selectedTags" :items="uniqueTags" label="Filtra per tag" multiple
+        @change="ManageTagsFilter" class="margin" style="width:200px" density="comfortable"
+        placeholder="Scrivi un tag..."></v-combobox>
 
     </v-row>
 
+
+
     <!-- Radio -->
     <v-row>
-      <v-col v-for="item in filteredRadios" :key="item.name" cols="12" sm="6" md="4" lg="2" xl="1">
+
+      <v-col v-for="item in filteredRadios" :key="item.name" cols="12" sm="6" md="4" lg="3" xl="2">
         <v-card class="card d-flex flex-column">
 
           <v-card-title>
@@ -40,6 +40,10 @@
           </v-card-title>
 
           <v-card-text style="font-size: 10px;">
+            <a :href="item.homepage" target="_blank">{{ item.homepage }}</a>
+          </v-card-text>
+
+          <v-card-text style="font-size: 10px; margin:0;padding:0">
             {{ item.tags.split(',').slice(0, 5).join(',') }}
           </v-card-text>
 
@@ -51,31 +55,25 @@
           </v-card-item>
 
           <v-card-item class="mt-auto">
-
             <div style="display: flex; justify-content: space-between; align-items: center;">
 
-              <div id="buttonplay">
-
-                <v-btn :color="activeRadio === item.name ? 'red' : 'blue'" variant="tonal"
+              <div id="buttonplay" style="flex-grow: 1;">
+                <v-btn block :color="activeRadio === item.name ? 'red' : 'blue'" variant="tonal"
                   @click="activeRadio === item.name ? StopAudio() : PlayAudio(item)">{{ activeRadio === item.name ?
                     'FERMA' : 'SUONA' }}</v-btn>
-
               </div>
 
-              <div id="favoriteButton">
-
-                <v-btn :color="isFavorite(item) ? 'red' : 'gray'" @click="addFavorite(item)">
+              <div id="favoriteButton" style="flex-grow: 1;">
+                <v-btn block :color="isFavorite(item) ? 'red' : 'gray'" @click="addFavorite(item)">
                   <v-icon>mdi-heart</v-icon>
                 </v-btn>
-
               </div>
 
-              <div id="player">
+              <div id="player" style="flex-grow: 1;">
                 <!-- Here will be the audio player -->
               </div>
 
             </div>
-
           </v-card-item>
 
         </v-card>
@@ -112,8 +110,16 @@
 }
 
 .custom-disabled {
-  opacity: 1 !important;
-  color: inherit !important;
+  opacity: 0.6;
+  pointer-events: none;
+}
+
+.gray-icon .v-icon {
+  color: #888;
+}
+
+.big-title {
+  font-size: 36px;
 }
 </style>
 
@@ -130,7 +136,9 @@ export default {
       activeRadio: null,
       currentLocation: null,
       favoriteRadios: [],
+      tagfilterRadios: [],
       showFilter: "all",
+      selectedTags: [],
     }
   },
 
@@ -144,11 +152,11 @@ export default {
         return;
       }
 
-      fetch('https://nl1.api.radio-browser.info/json/stations/search?limit=20' +
-        '&countrycode=IT')
+      fetch('https://nl1.api.radio-browser.info/json/stations/search?limit=200&countrycode=IT&hidebroken=true&order=clickcount&reverse=true')
         .then(response => response.json())
         .then(data => {
           this.radios = data;
+          this.removeDuplicateRadios(this.radios);
           this.saveInLocalStorage();
           console.log(this.radios);
         })
@@ -245,6 +253,32 @@ export default {
       this.showFilter = filter;
       console.log(filter);
     },
+
+    FilterByTag() {
+      this.tagfilterRadios = this.radios.filter(radio => radio.tags.includes(this.selectedTags));
+      this.FilterMode('tag');
+    },
+
+    ManageTagsFilter() {
+      if (this.selectedTags.length > 5) {
+        this.selectedTags = this.selectedTags.slice(0, 5);
+      }
+      if (this.selectedTags.length == 0) {
+        this.tagfilterRadios = [];
+        this.FilterMode('all');
+      }
+      else {
+        this.FilterByTag();
+      }
+    },
+
+    removeDuplicateRadios(radiosarr) {
+      const uniqueRadios = radiosarr.filter((radiosarr, index, self) =>
+        index === self.map(item => item.name).indexOf(radiosarr.name)
+      );
+      return uniqueRadios;
+    },
+
   },
 
   created() {
@@ -254,14 +288,29 @@ export default {
   computed: {
     filteredRadios() {
       if (this.showFilter === 'all') {
-        return this.radios;
+        return this.removeDuplicateRadios(this.radios);
       } else if (this.showFilter === 'favorite') {
-        return this.favoriteRadios;
+        return this.removeDuplicateRadios(this.favoriteRadios);
+      } else if (this.showFilter === 'tag') {
+        return this.removeDuplicateRadios(this.tagfilterRadios);
       } else {
         return [];
       }
     },
+
+    uniqueTags() {
+      const allTags = this.radios.flatMap(radio => radio.tags.split(','));
+      return [...new Set(allTags)].sort();
+    },
   },
 
+  watch: {
+    selectedTags: {
+      handler() {
+        this.ManageTagsFilter();
+      },
+      deep: true,
+    },
+  },
 }
 </script>
